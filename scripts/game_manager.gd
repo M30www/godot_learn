4a577a21d3366029705c8e3dab8ad184f727ecec
qwq Polestar <qaqmoss@gmail.com>
2024-11-20 01:22:50 +0800

    1.添加了捡起硬币的一系列操作 （设置为不可见，去除collider，播放音效，最后再释放硬币 2.添加了背景音乐，并设置为自动导入（死亡之后不会再重新播放 3.导出了exe文件

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,11 +1,9 @@
 extends Node
+@onready var score_label: Label = $ScoreLabel
+
+var score := 0
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func add_point():
+	score += 1
+	score_label.text = "You collected " + str(score) + " coins."
