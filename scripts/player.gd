bbb10883d62c532534ba91b2bb1378400276dd60
meowww <qaqmoss@gmail.com>
2024-11-21 00:17:45 +0800

    1.添加了二段跳功能 2.创建了一个新的仓库来放置演示的gif 3.编写了第一页wiki

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,17 +3,24 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
+# 尝试添加一个二段跳功能，通过空中和落地程维护个bool值来实现
+var CAN_DOUBLE_JUMP := true
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	# 在地上恢复二段跳状态
+	if is_on_floor():
+		CAN_DOUBLE_JUMP = true
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and CAN_DOUBLE_JUMP:
 		velocity.y = JUMP_VELOCITY
+		# 当不在地面使用跳跃的时候消耗跳跃次数（两次使用bool，多次可能需要维护一个int值..?
+		if !is_on_floor():
+			CAN_DOUBLE_JUMP = false
 
 	# 拿取运动向 -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
